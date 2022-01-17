1d761f8e5c447c8880ef1f569692a5699f086b08
Philip Chau <kyoc2020@gmail.com>
2022-01-17 01:10:21 -0500

    init_project, feat(stop_watch): use built-in timer for counting, whenever lap/reset is pressed, table view will be updated.

---
+++ MAD4114_review_exe/LapViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  LapViewCell.swift
+//  MAD4114_review_exe
+//
+//  Created by Philip Chau on 16/1/2022.
+//
+
+import UIKit
+
+class LapViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    @IBOutlet weak var time: UILabel!
+    @IBOutlet weak var lap: UILabel!
+    
+}
--- MAD4114_review_exe/ViewController.swift
+++ MAD4114_review_exe/ViewController.swift
@@ -7,13 +7,86 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return lapTime.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = lapTable.dequeueReusableCell(withIdentifier: "lap") as! LapViewCell
+        cell.time.text = lapTime[indexPath.row]
+        cell.lap.text = "lap \(indexPath.row + 1)"
+        return cell
+    }
+    
+    
+    var timer:Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        lapTable.delegate = self
+        lapTable.dataSource = self
     }
-
-
+    
+    var second:Int = 0
+    var minute:Int = 0
+    var minSec:Int = 0
+    var lapTime = [String]()
+    var counting:Bool = false
+    
+    @IBOutlet weak var minLabel: UILabel!
+    @IBOutlet weak var secLabel: UILabel!
+    @IBOutlet weak var minSecLabel: UILabel!
+    @IBOutlet weak var startBtn: UIButton!
+    @IBOutlet weak var lapBtn: UIButton!
+    @IBOutlet weak var lapTable: UITableView!
+    
+    
+    @IBAction @objc func start(_ sender: UIButton) {
+        counting = !counting
+        if counting {
+            timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(fire), userInfo: nil, repeats: true)
+            startBtn.setTitle("Stop", for: .normal)
+        }else{
+            timer.invalidate()
+            startBtn.setTitle("Start", for: .normal)
+            lapBtn.setTitle("Reset", for: .normal)
+        }
+    }
+    
+    @objc func fire () {
+        minSec += 1;
+        minSecLabel.text = "\(minSec)"
+        if minSec == 100 {
+            minSec = 0
+            second += 1
+            minSecLabel.text = "\(minSec)"
+            secLabel.text = "\(second)"
+            if second == 60 {
+                second = 0
+                minute += 1
+                secLabel.text = "\(second)"
+                minLabel.text = "\(minute)"
+            }
+        }
+    }
+    
+    @IBAction func lap(_ sender: Any) {
+        if counting {
+            lapTime.append("\(minute):\(second):\(minSec)")
+        }else{
+            second = 0
+            minute = 0
+            minSec = 0
+            minSecLabel.text = "\(minSec)"
+            secLabel.text = "\(second)"
+            minLabel.text = "\(minute)"
+            lapTime.removeAll()
+        }
+        lapTable.reloadData()
+    }
+    
+    
 }
 
